f1845703ca73f50c0dc711f3f1adf08352d5cd5e
Kevin-Liu-Du <liukevin412@gmail.com>
2024-03-14 17:47:32 -0600

    x

--- Tarea 3/Reto9/index.js
+++ Tarea 3/Reto9/index.js
@@ -1,122 +1,101 @@
-document.querySelector('#btnCalcular').addEventListener('click', evaluacion);
+function evaluarEncuesta() {
 
-function evaluacion() {
-    char = document.querySelector('#charla').value;
-    error_radio = validar_radio();
+    //Preguntas 1  y 2.
+    var pregunta1 = document.getElementById("r1").value
+    var pregunta2 = document.getElementById("r2").value
 
-    if (char== "" || error_radio == true) {
-        Swal.fire({
-            title: 'Atención',            
-            imageUrl: 'images/error.png',
-            width:450,
-            height:200,
-            iconColor: '#FFF',
-            position:'center',
-            confirmButtonColor: '#c82b2b',
-            color:'#000',
-            html: '<br><br> Tiene campos vacios. <br><br> Contestar todas las preguntas.',
+    //Codigo para saber la opcion escogida en el input radio de la pregunta 3.
+    var respuesta3 = ""
+    document.getElementsByName("opinion")
+        .forEach(radio => {
+            if (radio.checked) {
+                console.log(radio.value);
+                respuesta3 = radio.value
+            }
+        });
+
+    //Codigo para saber la opcion escogida en el input radio de la pregunta 4.
+    var respuesta4 = ""
+    document.getElementsByName("opinion2")
+        .forEach(radio => {
+            if (radio.checked) {
+                console.log(radio.value);
+                respuesta4 = radio.value
+            }
+        });
+
+    //Validacion de los datos ingresados
+    if (pregunta1 == "" || pregunta2 == "" || respuesta3 == "" || respuesta4 == "") {
+        swal.fire({
+            title: "Atención usuario",
+            text: "No deje campos vacios!",
+            icon: "warning",
         })
     } else {
-        
-        preg1 = document.querySelector('#pregunta1 input[type=radio]:checked').value;
-        preg2 = document.querySelector('#pregunta2 input[type=radio]:checked').value;
-        preg3 = document.querySelector('#pregunta3 input[type=radio]:checked').value;
-        preg4 = document.querySelector('#pregunta4 input[type=radio]:checked').value;
-        preg5 = document.querySelector('#pregunta5 input[type=radio]:checked').value;
 
+        //condicionales que determinan las opciones seleccionadas por el cliente.
+        var mensaje1 = "";
+        if (document.formulario.creatividad.checked == true && document.formulario.comunicar.checked == true && document.formulario.flexibilidad.checked == true) {
+            mensaje1 = "¡Gracias por confiar en nosotros y calificarnos bien en nuestros desempeño!";
+        } else if (document.formulario.creatividad.checked == false || document.formulario.comunicar.checked == false || document.formulario.flexibilidad.checked == false) {
+            mensaje1 = "Gracias por sus respuestas, tomaremos en cuenta sus respuestas para mejorar.";
+        } else if (document.formulario.creatividad.checked == false && document.formulario.comunicar.checked == false && document.formulario.flexibilidad.checked == false) {
+            mensaje1 = "Gracias por sus respuestas, lamentamos no haya tenido la mejor experiencia, lo tomaremos en cuenta para mejorar.";
+        }
 
+        var mensaje3 = "";
+        if (respuesta4 == "Buena") {
+            mensaje3 = "Agradecemos sinceramente su valoración positiva de nuestro desempeño. Seguiremos comprometidos en proporcionar una educación aún mejor para continuar guiando a nuestros estudiantes hacia el éxito. ";
+        } else if (respuesta4 == "Regular") {
+            mensaje3 = "Agradecemos su evaluación de nuestro desempeño como regular. Tomamos en serio sus comentarios y nos comprometemos a trabajar arduamente para mejorar y superar sus expectativas en el futuro.";
+        } else {
+            mensaje3 = "Lamentamos sinceramente su evaluación negativa de nuestro desempeño. Valoramos sus comentarios y nos comprometemos a tomar medidas inmediatas para abordar cualquier preocupación que haya surgido.";
+        }
 
-        numres1 = parseInt(preg1);
-        numres2 = parseInt(preg2);
-        numres3 = parseInt(preg3);
-        numres4 = parseInt(preg4);
-        numres5 = parseInt(preg5);
+        var mensaje2 = "";
+        if (respuesta3 == "Si") {
+            mensaje2 = "Nos complace enormemente recibir su confirmación sobre la utilidad de nuestras evaluaciones docentes. Apreciamos sinceramente su compromiso con la mejora continua de la calidad de la enseñanza y la experiencia de aprendizaje.";
+        } else {
+            mensaje2 = "Agradecemos sinceramente su retroalimentación. Lamentamos que nuestras evaluaciones docentes no hayan cumplido con sus expectativas.";
+        }
 
-
-
-
-        ptnSubTotal = numres1 + numres2 + numres3 + numres4 + numres5;
-        console.log(ptnSubTotal);
-
-        ptnTotal = ptnSubTotal / 25;
-        console.log(ptnTotal);
-
-        porcFinal = ptnTotal * 100;
-        console.log(porcFinal);
-
-        Swal.fire({
-            title: 'Evaluación obtenida: ' + porcFinal + '%',
-            icon: 'info',
-            iconColor: '#FFF',
-            color:'#000',
-            confirmButtonColor: '#c82b2b',
-            html: '<br><br> Calificacion brindada a la evaluación del docente: ' + char,
+        //desplegable aparecera cuando el cliente da clic al boton Enviar
+        swal.fire({
+            title: "Respuestas enviadas",
+            text: "Gracias por realizar nuestra encuesta",
+            //icon: "success",
+            html: "<iframe src='https://embed.lottiefiles.com/animation/123492' width='440' height='380' > < /iframe>"
         })
-
-    };
-
-}
-
-let validar_radio = () => {
-    let error = false;
-    let error_radio = false;
-    let inpRadio1 = document.querySelector('#pregunta1 input[type=radio]:checked');
-    let inpRadio2 = document.querySelector('#pregunta2 input[type=radio]:checked');
-    let inpRadio3 = document.querySelector('#pregunta3 input[type=radio]:checked');
-    let inpRadio4 = document.querySelector('#pregunta4 input[type=radio]:checked');
-    let inpRadio5 = document.querySelector('#pregunta5 input[type=radio]:checked');
-    if (!inpRadio1 || !inpRadio2 || !inpRadio3 || !inpRadio4 || !inpRadio5) {
-        error = true;
-        error_radio = true;
-    } else {
-        error_radio = false;
+        //resultado de la encuesta
+        document.getElementById("res0").innerHTML = "¡Gracias por sus respuestas!";
+        document.getElementById("res1").innerHTML = mensaje2
+        document.getElementById("res2").innerHTML = mensaje3
+        document.getElementById("res3").innerHTML = mensaje1
     }
-    return error_radio;
 }
 
+function limpiarFormulario() {
+    document.getElementById("r1").value = "";
+    document.getElementById("r2").value = "";
 
+    var opcionesRadio3 = document.getElementsByName("opinion");
+    opcionesRadio3.forEach(radio => (radio.checked = false));
 
-function btnlimpiar() {
-    document.getElementById('charla').value = "";
+    var opcionesRadio4 = document.getElementsByName("opinion2");
+    opcionesRadio4.forEach(radio => (radio.checked = false));
 
-    document.getElementById("a").checked = false;
-        document.getElementById("b").checked = false;
-            document.getElementById("c").checked = false;
-                document.getElementById("d").checked = false;
-                    document.getElementById("e").checked = false;
+    document.getElementById("comunicar").checked = false;
+    document.getElementById("creatividad").checked = false;
+    document.getElementById("flexibilidad").checked = false;
+    document.getElementById("Malo").checked = false;
+    document.getElementById("Regular").checked = false;
+    document.getElementById("Bueno").checked = false;
+    document.getElementById("Muy Bueno").checked = false;
+    document.getElementById("Excelente").checked = false;
 
+    document.getElementById("res0").innerHTML = "";
+    document.getElementById("res1").innerHTML = "";
+    document.getElementById("res2").innerHTML = "";
+    document.getElementById("res3").innerHTML = "";
 
-    document.getElementById("f").checked = false;
-        document.getElementById("g").checked = false;
-            document.getElementById("h").checked = false;
-                document.getElementById("i").checked = false;
-                    document.getElementById("j").checked = false;
-
-
-    document.getElementById("k").checked = false;
-        document.getElementById("l").checked = false;
-            document.getElementById("m").checked = false;
-                document.getElementById("n").checked = false;
-                    document.getElementById("o").checked = false;
-
-
-
-    document.getElementById("p").checked = false;
-        document.getElementById("q").checked = false;
-            document.getElementById("r").checked = false;
-                document.getElementById("s").checked = false;
-                    document.getElementById("t").checked = false;  
-
-
-
-    document.getElementById("u").checked = false;
-        document.getElementById("w").checked = false;
-            document.getElementById("x").checked = false;
-                document.getElementById("y").checked = false;
-                    document.getElementById("z").checked = false;  
 }
-
-
-
-
-

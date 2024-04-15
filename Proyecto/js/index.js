2fab970b799bc74b6424ec0a5e00047de819cdd5
Kevin-Liu-Du <liukevin412@gmail.com>
2024-04-14 22:12:55 -0600

    x

---
+++ Proyecto/js/index.js
@@ -0,0 +1,103 @@
+'use extrict';
+
+//carga de la funcion init y se ejecuta al entrar a la web load
+window.addEventListener('load', init, false);
+
+//funcion inicial
+function init() {
+    let nombre = document.getElementById('nombreTxt');
+    let apellido = document.getElementById('apellidoTxt');
+    let email = document.getElementById('emailTxt');
+    let numero = document.getElementById('numberTxt');
+    let cedula = document.getElementById('cedulaTxt');
+    let fecha = document.getElementById('fechaTxt');
+    let evento = document.getElementById('eventosTxt'); 
+    let alerta = document.getElementById('mensajeAlert');
+    let btnEnviar = document.getElementById('btnSend');
+    let expressionEmail = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+
+    //funcion del boton Enviar
+    btnEnviar.onclick = function() {
+        nombre = nombreTxt.value;
+        apellido = apellidoTxt.value;
+        email = emailTxt.value;
+        numero = numberTxt.value;
+        cedula = cedulaTxt.value;
+        fecha = fechaTxt.value;
+        evento = eventosTxt.value;
+
+        //condicionales anidadas para validar cada campo del form 
+        if (nombre === '' && apellido === '' && email === '' && numero === '' && cedula === '' && fecha === '' && evento === '') {
+            alerta.textContent = 'Debe llenar todos los campos';
+            alerta.classList.add('alertaRoja');
+            alerta.classList.remove('alertaVerde');
+        } else if (nombre === '') {
+            alerta.textContent = 'El campo nombre esta vacío';
+            alerta.classList.add('alertaRoja');
+            alerta.classList.remove('alertaVerde');
+        } else if (apellido === '') {
+            alerta.textContent = 'El campo apellido esta vacío';
+            alerta.classList.add('alertaRoja');
+            alerta.classList.remove('alertaVerde');
+        } else if (email === '') {
+            alerta.textContent = 'El campo email esta vacío';
+            alerta.classList.add('alertaRoja');
+            alerta.classList.remove('alertaVerde');
+        } else if (expressionEmail.test(email) === false) {
+            alerta.textContent = 'Email invalido';
+            alerta.classList.add('alertaRoja');
+            alerta.classList.remove('alertaVerde');
+        } else if (numero === '') {
+            alerta.textContent = 'El campo teléfono esta vacío';
+            alerta.classList.add('alertaRoja');
+            alerta.classList.remove('alertaVerde');
+
+        } else if (cedula === '') {
+            alerta.textContent = 'El campo cedula esta vacío';
+            alerta.classList.add('alertaRoja');
+            alerta.classList.remove('alertaVerde');
+        } else if (fecha === '') {
+            alerta.textContent = 'El campo fecha esta vacío';
+            alerta.classList.add('alertaRoja');
+            alerta.classList.remove('alertaVerde');
+        } else if (evento === '') {
+            alerta.textContent = 'El campo evento esta vacío';
+            alerta.classList.add('alertaRoja');
+            alerta.classList.remove('alertaVerde');
+
+        } else {
+            //si NO existe error en los campos validados se envian los datos al servidor de correo 
+            alerta.textContent = 'Reservación Enviado';
+            alerta.classList.add('alertaVerde');
+            alerta.classList.remove('alertaRoja');
+            //service ID/template ID/ID form/public key cuenta
+            //el id del formulario es #form1
+            //dejarlos vacios para luego completarlos...
+            emailjs.sendForm("service_1thdu2i","template_4r65xyj","#form", "Zk0pZ2Vmcm9xWyDKC");
+            //carga de la funcion limpiar
+            limpiar();
+        }
+    }
+
+    function limpiar() {
+        nombreTxt.value = '';
+        apellidoTxt.value = '';
+        emailTxt.value = '';
+        numberTxt.value = '';
+        cedulaTxt.value = '';
+        fechaTxt.value = '';
+        eventosTxt.value = ''
+    }
+}
+
+let hoy = new Date();
+        let dd = String(hoy.getDate()).padStart(2, '0');
+        let mm = String(hoy.getMonth() + 1).padStart(2, '0'); // Enero es 0
+        let yyyy = hoy.getFullYear();
+        let fechaHoy = yyyy + '-' + mm + '-' + dd;
+
+        // Configurar el atributo 'min' de los campos de fecha para que no puedan seleccionar una fecha anterior a hoy
+        let camposFecha = document.querySelectorAll('input[type="date"]');
+        camposFecha.forEach(function(input) {
+            input.setAttribute('min', fechaHoy);
+        });

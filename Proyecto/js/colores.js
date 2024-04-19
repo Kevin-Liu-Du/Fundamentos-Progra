82d8792558b183201f91f99a2b3a19cab3a6133f
Kevin-Liu-Du <liukevin412@gmail.com>
2024-04-18 19:05:07 -0600

    terminado

--- Proyecto/js/colores.js
+++ Proyecto/js/colores.js
@@ -1,29 +1,29 @@
 function c1() {
-    document.getElementById("vehiculo").src = "img/rojo.png";
+    document.getElementById("vehiculo").src = "../img/rojo.png";
     document.getElementById("color").innerHTML = "RED MICA METALLIC (3R3)";
 }
 
 function c2() {
-    document.getElementById("vehiculo").src = "img/azul.png";
+    document.getElementById("vehiculo").src = "../img/azul.png";
     document.getElementById("color").innerHTML = "ATTITUDE BLACK (218)";
 }
 
 function c3() {
-    document.getElementById("vehiculo").src = "img/gris.png";
+    document.getElementById("vehiculo").src = "../img/gris.png";
     document.getElementById("color").innerHTML = "SUPER GREY II (040)";
 }
 
 function c4() {
-    document.getElementById("vehiculo").src = "img/naranja.png";
+    document.getElementById("vehiculo").src = "../img/naranja.png";
     document.getElementById("color").innerHTML = "ORANGE METALLIC (4R8)";
 }
 
 function c5() {
-    document.getElementById("vehiculo").src = "img/verde.png";
+    document.getElementById("vehiculo").src = "../img/verde.png";
     document.getElementById("color").innerHTML = "CITRUS MICA METALLIC (6W2)";
 }
 
 function c6() {
-    document.getElementById("vehiculo").src = "img/morado.png";
+    document.getElementById("vehiculo").src = "../img/morado.png";
     document.getElementById("color").innerHTML = "PURPLE METALLIC (4T1)";
 }
